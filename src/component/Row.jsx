854b046f70921610908f8cd12e2088630c2c575e
ulrichtoussom <ulrichtoussom@gmail.com>
2025-12-24 10:59:21 +0100

    Ajout du chronometre heure actuelle et mise a forme de notre composant ProductTable et Row (ProductTable.css) et pour le chronometre(DisplayTime.css)

--- src/component/Row.jsx
+++ src/component/Row.jsx
@@ -6,8 +6,28 @@ import {Check, XCircleFill}  from 'react-bootstrap-icons'
 
 export default function Row({userID , title, check}){
 
-    return(
+    /* return(
         <tr>< td>{userID}</td><td>{title}</td><td>{ check ? <Check color='green' size={20}/> : <XCircleFill color='red'/> } </td></tr>
+    ) */
+
+    return(
+        <div className='product-row'>
+
+            <div className="product-category">{userID}</div>
+            <div className="product-name" style={{ fontWeight: '600' }}>
+                {title}
+            </div>
+
+            <div>
+                <span className={`status-badge ${ check ? 'status-in' : 'status-out'}`}>
+                {check ? <Check color='green' size={20}/> : <XCircleFill color='red'/>}
+                </span>
+            </div>
+
+
+        </div>
     )
+
+
 }
 
